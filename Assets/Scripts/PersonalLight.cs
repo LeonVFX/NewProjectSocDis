7562607b32ef2b912c0117d2791707a2f2df0899
LeonVFX <54697940+LeonVFX@users.noreply.github.com>
2021-03-11 17:35:18 +0000

    Flashlight
    - Add flashlight implementation

--- Assets/Scripts/PersonalLight.cs
+++ Assets/Scripts/PersonalLight.cs
@@ -15,11 +15,12 @@ public class PersonalLight : MonoBehaviour
         Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(pivot);
         Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
-        Vector3 dir = Vector3.Normalize(new Vector3(mouseOnScreen.x, 0.0f, mouseOnScreen.y) - new Vector3(positionOnScreen.x, 0.0f, positionOnScreen.y));
-        transform.position = pivot + (dir * radius);
-
+        // Light angle
         float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle - Camera.main.transform.rotation.y, 0f));
+        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle - 45, 0f));
+
+        // Orbit
+        transform.position = pivot + (radius * transform.forward);
     }
 
     float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
